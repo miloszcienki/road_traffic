9f222f1aa349d1bc4453dd4d421b38912e1fdc93
miloszcienki <miloszcienki@tlen.pl>
2023-10-30 22:02:54 +0100

    application window

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Main extends JFrame{
+    public Main(){
+        setTitle("Road Traffic");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(1280,1024);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+       new Main();
+    }
+}
+
+
